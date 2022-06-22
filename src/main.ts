09ff6b07119e3151b15065187ce760ceee58ec6f
Ernesto Alejo <ernestokarim@gmail.com>
2022-06-22 13:46:35 +0200

    Implementación inicial basada en la GitHub Action pareja.

---
+++ src/main.ts
@@ -0,0 +1,34 @@
+
+import * as fs from 'fs'
+import * as path from 'path'
+import * as os from 'os'
+
+
+async function main(): Promise<void> {
+  const npmrc = path.resolve(process.cwd(), '.npmrc')
+
+  let lines: string[] = []
+  if (fs.existsSync(npmrc)) {
+    lines.push(fs.readFileSync(npmrc, 'utf8'))
+  }
+
+  if (process.env.FONTAWESOME_TOKEN) {
+    console.log('* Configure Font Awesome')
+    lines.push(`@fortawesome:registry=https://npm.fontawesome.com/`)
+    lines.push(`//npm.fontawesome.com/:_authToken=${process.env.FONTAWESOME_TOKEN}`)
+  }
+
+  if (process.env.GITHUB_TOKEN) {
+    console.log('* Configure GitHub Packages')
+    lines.push(`@altipla-consulting:registry=https://npm.pkg.github.com`)
+    lines.push(`//npm.pkg.github.com/:_authToken=${process.env.GITHUB_TOKEN}`)
+    lines.push(`always-auth=true`)
+  }
+
+  lines.push('git-tag-version=false')
+  
+  console.log('* Write .npmrc')
+  fs.writeFileSync(npmrc, lines.join(os.EOL), 'utf-8')
+}
+
+main()
